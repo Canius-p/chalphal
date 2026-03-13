e31d37d25e5b413bcb7f08c7c8dd26e81a7159a6
Canius-p <boiinventer@gmail.com>
2026-03-13 22:15:35 +0545

    initial setup

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chalfal The Talk</title>
+    <?php include('./client/commonFiles.php') ?>
+</head>
+
+<body>
+    <?php include(',.client/header.php') ?>
+</body>
+
+</html>
